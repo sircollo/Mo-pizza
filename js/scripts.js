150f90ad23ea90d9b8e45f8f5d90411bfff4b44b
Collins Sirwani <sircollo71@gmail.com>
2022-03-21 00:12:04 +0300

    Create constructor functions for order and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
+function PizzaOrder(size, price, crust, crustPrice) {
+  this.size = {
+    size: size,
+    price: price,
+  };
+  this.crust = {
+    crustName: crust,
+    price: crustPrice,
+  };
+  this.toppings = [];
+}
 
+function Topping(name, price) {
+  this.toppingName = name;
+  this.price = price;
+}
 
-$(document).ready(function(event){
-//form validation
+$(document).ready(function (event) {
+  //form validation
   $("#new-order").submit(function (event) {
     event.preventDefault();
 
@@ -15,9 +30,8 @@ $(document).ready(function(event){
 
     if (!isChecked) {
       alert("Choose a Topping");
+    } else {
+      $(".order-button", this).text("Continue Shopping");
     }
   });
-
-
-
 });
