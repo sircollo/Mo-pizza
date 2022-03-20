ad403aa5c48e235450bdd86032a10a608a3ef1d2
Collins Sirwani <sircollo71@gmail.com>
2022-03-21 00:26:13 +0300

    Declare object variables for different items

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,38 @@ function Topping(name, price) {
   this.toppingName = name;
   this.price = price;
 }
+var toppingCost = 0;
+var pizzaPrices = {
+  Small: 500,
+  medium: 800,
+  large: 1200,
+};
+var crustPrices = {
+  crispy: 100,
+  stuffed: 150,
+  Glutten_Free: 200,
+};
+var smallTopPrices = {
+  beef: 120,
+  chicken: 130,
+  mushroom: 150,
+  herbs: 100,
+};
+
+var mediumTopPrices = {
+  beef: 170,
+  chicken: 170,
+  mushroom: 200,
+  herbs: 120,
+};
+var largeTopPrices = {
+  beef: 250,
+  chicken: 250,
+  mushroom: 300,
+  herbs: 150,
+};
+
+
 
 $(document).ready(function (event) {
   //form validation
